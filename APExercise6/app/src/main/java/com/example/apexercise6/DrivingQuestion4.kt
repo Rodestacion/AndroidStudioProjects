465b87c42cdb60890cf1069964a1e31999dd8e9e
Rodney Estacion <rodney.estacion@gmail.com>
2023-06-03 10:29:22 +0800

    update activity file

---
+++ APExercise6/app/src/main/java/com/example/apexercise6/DrivingQuestion4.kt
@@ -0,0 +1,11 @@
+package com.example.apexercise6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DrivingQuestion4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_driving_question4)
+    }
+}
---
+++ APExercise6/app/src/main/java/com/example/apexercise6/DrivingQuestion5.kt
@@ -0,0 +1,11 @@
+package com.example.apexercise6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DrivingQuestion5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_driving_question5)
+    }
+}
