2a46aed1043aebc7704637c2f6aa12bb7ecaa895
Rodney Estacion <rodney.estacion@gmail.com>
2023-06-10 19:23:00 +0800

    update activity file

--- UIBasic2/app/src/main/java/com/example/uibasic2/MainActivity.kt
+++ UIBasic2/app/src/main/java/com/example/uibasic2/MainActivity.kt
@@ -2,12 +2,100 @@ package com.example.uibasic2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.uibasic2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        //Toggle Button
+        binding.toggleButton.setOnClickListener {
+            if(binding.toggleButton.isChecked){
+                Toast.makeText(applicationContext, "On", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(applicationContext, "Off", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        //Radio button
+
+        binding.radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when(checkedId){
+                R.id.radioButton3->{
+                    Toast.makeText(applicationContext, "Radio Button 3", Toast.LENGTH_SHORT).show()
+                }
+                R.id.radioButton4->{
+                    Toast.makeText(applicationContext, "Radio Button 4", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        //Checkbox
+        binding.checkBox.setOnClickListener {
+            if(binding.checkBox.isChecked){
+                Toast.makeText(applicationContext, "Checked!", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(applicationContext, "Uncghecked", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        //progress bar
+        binding.button.setOnClickListener {
+            binding.progressBar.progress +=10
+        }
+
+        //Spinner
+        val spinnerItems = arrayOf("Item 1", "Item 2", "Item 3")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item,spinnerItems)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinner.adapter =adapter
+
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem = spinnerItems[position]
+                Toast.makeText(applicationContext, selectedItem, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Toast.makeText(applicationContext, "Nothing is selected", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        //custom spinner
+        val adapter2 = ArrayAdapter(this,R.layout.custom_item,spinnerItems)
+        adapter2.setDropDownViewResource(R.layout.custom_item)
+        binding.spinner2.adapter =adapter2
+
+        binding.spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem = spinnerItems[position]
+                Toast.makeText(applicationContext, selectedItem, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Toast.makeText(applicationContext, "Nothing is selected", Toast.LENGTH_SHORT).show()
+            }
+
+        }
 
 
 
--- UIBasic2/app/src/main/java/com/example/uibasic2/MainActivity2.kt
+++ UIBasic2/app/src/main/java/com/example/uibasic2/MainActivity2.kt
@@ -1,11 +1,44 @@
 package com.example.uibasic2
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.uibasic2.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var  binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button2.setOnClickListener {
+            showDialog()
+        }
+
+        binding.imageButton.setOnClickListener{
+            showDialog()
+        }
+
+
+    }
+
+    private fun showDialog(){
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("this is a dialog box")
+        alertDialogBuilder.setMessage("This is a Dialog box!")
+        alertDialogBuilder.setPositiveButton("OK"){_:DialogInterface, _:Int ->  // "_" indicate that parameter is not used
+            Toast.makeText(applicationContext,"Ok is Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        alertDialogBuilder.setNegativeButton("Cancelled"){_:DialogInterface, _:Int ->  // "_" indicate that parameter is not used
+            Toast.makeText(applicationContext,"Cancelled is Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        alertDialogBuilder.create().show()
+
+
     }
 }
