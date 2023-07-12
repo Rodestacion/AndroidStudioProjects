bf6c57f41f01431414563ecdd9dccf7c675c1eae
Rodney <rodney.estacion@gmail.com>
2023-07-12 19:23:21 +0800

    update activity file

---
+++ RoomMultipleTables/app/src/main/java/com/data/roommultipletables/activity/ProductsActivity.kt
@@ -0,0 +1,12 @@
+package com.data.roommultipletables.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.data.roommultipletables.R
+
+class ProductsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_products)
+    }
+}
