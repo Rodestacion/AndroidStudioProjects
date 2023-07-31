0537f2afb35f1beb9edb6e07bf39ad2e558d808d
Rodney <rodney.estacion@gmail.com>
2023-08-01 04:30:42 +0800

    update activity file

--- APExercise13/app/src/main/java/com/data/apexercise13/MainActivity.kt
+++ APExercise13/app/src/main/java/com/data/apexercise13/MainActivity.kt
@@ -2,10 +2,151 @@ package com.data.apexercise13
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.Manifest
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.data.apexercise13.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var startActivityLauncher:ActivityResultLauncher<Intent>
+    private lateinit var galleryLauncher: ActivityResultLauncher<String>
+    private lateinit var sharedPreferences: SharedPreferences
+//    private lateinit var myProfile: ArrayList<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sharedPreferences = getSharedPreferences("MyPrefs", AppCompatActivity.MODE_PRIVATE)
+
+        var userProfile = sharedPreferences
+        var profileDetail = arrayOf(userProfile.getString("image",""),
+            userProfile.getString("name",""),
+            userProfile.getString("location",""),
+            userProfile.getString("followers","0"),
+            userProfile.getString("following","0"),
+            userProfile.getString("email",""),
+            userProfile.getString("phone",""),
+            userProfile.getString("twitter",""),
+            userProfile.getString("facebook",""))
+//        myProfile = profileDetail.toString() as ArrayList<String>
+        Toast.makeText(applicationContext, profileDetail.toString(), Toast.LENGTH_SHORT).show()
+
+
+        val f1 = ProfileFragment()
+        val f2 = EditProfileFragment()
+
+
+        var myBundle = Bundle()
+        myBundle.putStringArray("profile",profileDetail as Array<String>)
+//        myBundle.putStringArrayList("profile", myProfile)
+        f1.arguments = myBundle
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(binding.fragmentContainerView.id,f1)
+            commit()
+        }
+
+        f1.onEditProfile={
+            supportFragmentManager.beginTransaction().apply {
+                replace(binding.fragmentContainerView.id,f2)
+                commit()
+            }
+            binding.btnImage.visibility = View.VISIBLE
+        }
+
+        f2.onSaveProfile = {
+            supportFragmentManager.beginTransaction().apply {
+                replace(binding.fragmentContainerView.id,f1)
+                commit()
+            }
+            binding.btnImage.visibility = View.INVISIBLE
+        }
+
+        startActivityLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){result: ActivityResult ->
+            if(result.resultCode == RESULT_OK){
+                val bitmap = (result.data?.extras?.get("data"))as? Bitmap ?: return@registerForActivityResult
+
+                Glide.with(this)
+                    .load(bitmap)
+                    .centerCrop()
+                    .circleCrop()
+                    //.into(binding.imageView)
+            }
+        }
+
+        galleryLauncher = registerForActivityResult(
+            ActivityResultContracts.GetContent()
+        ){uri: Uri? ->
+            if(uri != null){
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+                //binding.imageView.setImageBitmap(bitmap)
+                Glide.with(this)
+                    .load(bitmap)
+                    .centerCrop()
+                    .circleCrop()
+                    //.into(binding.imageView)
+            }
+        }
+
+//        binding.imageButton.setOnClickListener {
+//            showDialog()
+//        }
+
+    }
+
+    private fun showDialog() {
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setTitle("Image Selection")
+        dialogBuilder.setMessage("Choose the source of your image")
+
+        dialogBuilder.setPositiveButton("Camera"){dialog,_->
+            if(ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),100)
+            }else{
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityLauncher.launch(intent)
+            }
+
+            dialog.dismiss()
+        }
+
+        dialogBuilder.setNegativeButton("Gallery"){dialog,_->
+            galleryLauncher.launch("image/*")
+
+        }
+
+
+        val dialog = dialogBuilder.create()
+        dialog.show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 100 && grantResults.isNotEmpty()&&grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityLauncher.launch(intent)
+        }
     }
 }
--- APExercise13/app/src/main/java/com/data/apexercise13/ProfileFragment.kt
+++ APExercise13/app/src/main/java/com/data/apexercise13/ProfileFragment.kt
@@ -1,59 +1,40 @@
 package com.data.apexercise13
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.data.apexercise13.databinding.FragmentProfileBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentProfileBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+    var onEditProfile:((Int)->Unit)?=null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
+    ): View {
+        binding = FragmentProfileBinding.inflate(layoutInflater,container,false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        var myArguments = arguments
+        var mainArray = myArguments?.getStringArrayList("profile")
+
+        Toast.makeText(context, mainArray.toString(), Toast.LENGTH_SHORT).show()
+
+        binding.apply {
+            btnEditProfile.setOnClickListener {
+                onEditProfile?.invoke(0)
             }
+        }
+
+
+        return binding.root
     }
 }
