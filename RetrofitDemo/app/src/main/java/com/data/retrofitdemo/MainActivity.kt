b12ab15d9304b0c8047d9c92f26b9ef97ee6a9fb
Rodney <rodney.estacion@gmail.com>
2023-08-01 04:32:20 +0800

    update activity file

--- RetrofitDemo/app/src/main/java/com/data/retrofitdemo/MainActivity.kt
+++ RetrofitDemo/app/src/main/java/com/data/retrofitdemo/MainActivity.kt
@@ -1,16 +1,51 @@
 package com.data.retrofitdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.data.retrofitdemo.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val BASE_URL = "https://jsonplaceholder.typicode.com/"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        getAllComments()
 
+    }
+
+    private fun getAllComments(){
+        val api = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MyApi::class.java)
+
+        api.getComments().enqueue(object : Callback<List<Comments>> {
+            override fun onResponse(
+                call: Call<List<Comments>>,
+                response: Response<List<Comments>>
+            ) {
+                if(response.isSuccessful){
+                    for(data in response.body()!!){
+                        Log.d("MYDATA",data.name)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<Comments>>, t: Throwable) {
+                Log.d("MYDATA","ERROR")
+            }
+
+        })
     }
 }
--- RetrofitDemo/app/src/main/java/com/data/retrofitdemo/MyApi.kt
+++ RetrofitDemo/app/src/main/java/com/data/retrofitdemo/MyApi.kt
@@ -1,4 +1,13 @@
 package com.data.retrofitdemo
 
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface MyApi {
+    @GET("comments")
+    fun getComments():Call<List<Comments>>
+
+//    @GET("users")
+//    fun getUsers():Call<List<Comments>>
+
 }
