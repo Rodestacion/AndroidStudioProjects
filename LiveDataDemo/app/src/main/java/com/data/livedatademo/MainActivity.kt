8c928765792bd011ec911674389916982259c047
Rodney <rodney.estacion@gmail.com>
2023-08-09 03:57:42 +0800

    update activity file

---
+++ LiveDataDemo/app/src/main/java/com/data/livedatademo/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.data.livedatademo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.data.livedatademo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+
+        //add observer
+        viewModel.name.observe(this, Observer {name->
+            binding.txtName.text = name.toString()
+        })
+
+        viewModel.age.observe(this, Observer {age->
+            binding.txtAge.text = age.toString()
+        })
+
+        binding.button.setOnClickListener {
+            viewModel.updateName(binding.etName.text.toString())
+            //viewModel.updateAge(binding.etAge.toString().toInt())
+            viewModel.updateAge(binding.etAge.toString())
+        }
+
+
+
+    }
+}
--- LiveDataDemo/app/src/main/java/com/data/livedatademo/MainActivityViewModel.kt
+++ LiveDataDemo/app/src/main/java/com/data/livedatademo/MainActivityViewModel.kt
@@ -1,4 +1,34 @@
 package com.data.livedatademo
 
-class MainActivityViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel:ViewModel() {
+    //name - private, immutable
+    private val _name = MutableLiveData<String>()
+    val name : LiveData<String> get() =_name
+
+    //age - private, immutable
+//    private val _age = MutableLiveData<Int>()
+//    val age: LiveData<Int> get() =_age
+    private val _age = MutableLiveData<String>()
+    val age: LiveData<String> get() =_age
+
+    //updating
+    fun updateName(name:String){
+        _name.value = name
+    }
+
+//    fun updateAge(age:Int){
+//        _age.value = age
+//    }
+    fun updateAge(age:String){
+        _age.value = age
+    }
+
+    override fun onCleared(){
+        super.onCleared()
+    }
+
 }
