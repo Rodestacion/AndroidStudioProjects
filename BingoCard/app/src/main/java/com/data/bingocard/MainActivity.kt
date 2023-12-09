db303b5b38b5ceba6769f4ac939c34fd972561d1
Rodney <rodney.estacion@gmail.com>
2023-12-09 19:04:00 +0800

    update project code

--- BingoCard/app/src/main/java/com/data/bingocard/MainActivity.kt
+++ BingoCard/app/src/main/java/com/data/bingocard/MainActivity.kt
@@ -1,15 +1,113 @@
 package com.data.bingocard
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.data.bingocard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+        setLaneB1Background(viewModel.laneB1)
+
+        binding.laneB1.setOnClickListener {
+            if (viewModel.laneB1==0){
+                viewModel.laneB1Gray()
+                setLaneB1Background(viewModel.laneB1)
+            }else{
+                viewModel.laneB1White()
+                setLaneB1Background(viewModel.laneB1)
+            }
+        }
+
+        binding.laneB2.setOnClickListener {
+            if (viewModel.laneB2==0){
+                viewModel.laneB2Gray()
+                setLaneB2Background(viewModel.laneB2)
+            }else{
+                viewModel.laneB2White()
+                setLaneB2Background(viewModel.laneB2)
+            }
+        }
+
+        binding.laneB3.setOnClickListener {
+            if (viewModel.laneB3==0){
+                viewModel.laneB3Gray()
+                setLaneB3Background(viewModel.laneB3)
+            }else{
+                viewModel.laneB3White()
+                setLaneB3Background(viewModel.laneB3)
+            }
+        }
+
+        binding.laneB4.setOnClickListener {
+            if (viewModel.laneB4==0){
+                viewModel.laneB4Gray()
+                setLaneB4Background(viewModel.laneB4)
+            }else{
+                viewModel.laneB4White()
+                setLaneB4Background(viewModel.laneB4)
+            }
+        }
+
+        binding.laneB5.setOnClickListener {
+            if (viewModel.laneB5==0){
+                viewModel.laneB5Gray()
+                setLaneB5Background(viewModel.laneB5)
+            }else{
+                viewModel.laneB5White()
+                setLaneB5Background(viewModel.laneB5)
+            }
+        }
     }
+
+    private fun setLaneB1Background(laneB1: Int) {
+        if(laneB1 == 0){
+            binding.laneB1.setBackgroundColor(Color.WHITE)
+        }else{
+            binding.laneB1.setBackgroundColor(Color.GRAY)
+        }
+    }
+
+    private fun setLaneB2Background(laneB2: Int) {
+        if(laneB2 == 0){
+            binding.laneB2.setBackgroundColor(Color.WHITE)
+        }else{
+            binding.laneB2.setBackgroundColor(Color.GRAY)
+        }
+    }
+
+    private fun setLaneB3Background(laneB3: Int) {
+        if(laneB3 == 0){
+            binding.laneB3.setBackgroundColor(Color.WHITE)
+        }else{
+            binding.laneB3.setBackgroundColor(Color.GRAY)
+        }
+    }
+
+    private fun setLaneB4Background(laneB4: Int) {
+        if(laneB4 == 0){
+            binding.laneB4.setBackgroundColor(Color.WHITE)
+        }else{
+            binding.laneB4.setBackgroundColor(Color.GRAY)
+        }
+    }
+
+    private fun setLaneB5Background(laneB5: Int) {
+        if(laneB5 == 0){
+            binding.laneB5.setBackgroundColor(Color.WHITE)
+        }else{
+            binding.laneB5.setBackgroundColor(Color.GRAY)
+        }
+    }
+
 }
+
---
+++ BingoCard/app/src/main/java/com/data/bingocard/MainActivityViewModel.kt
@@ -0,0 +1,244 @@
+package com.data.bingocard
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel:ViewModel() {
+    var laneB1:Int = 0
+    var laneB2:Int = 0
+    var laneB3:Int = 0
+    var laneB4:Int = 0
+    var laneB5:Int = 0
+
+    var laneI1:Int = 0
+    var laneI2:Int = 0
+    var laneI3:Int = 0
+    var laneI4:Int = 0
+    var laneI5:Int = 0
+
+    var laneN1:Int = 0
+    var laneN2:Int = 0
+    var laneN3:Int = 0
+    var laneN4:Int = 0
+    var laneN5:Int = 0
+
+    var laneG1:Int = 0
+    var laneG2:Int = 0
+    var laneG3:Int = 0
+    var laneG4:Int = 0
+    var laneG5:Int = 0
+
+    var laneO1:Int = 0
+    var laneO2:Int = 0
+    var laneO3:Int = 0
+    var laneO4:Int = 0
+    var laneO5:Int = 0
+    
+    //Set to Gray Background
+    fun laneB1Gray(){
+        laneB1 = 1
+    }
+
+    fun laneB2Gray(){
+        laneB2 = 1
+    }
+
+    fun laneB3Gray(){
+        laneB3 = 1
+    }
+
+    fun laneB4Gray(){
+        laneB4 = 1
+    }
+
+    fun laneB5Gray(){
+        laneB5 = 1
+    }
+
+    fun laneI1Gray(){
+        laneI1 = 1
+    }
+
+    fun laneI2Gray(){
+        laneI2 = 1
+    }
+
+    fun laneI3Gray(){
+        laneI3 = 1
+    }
+
+    fun laneI4Gray(){
+        laneI4 = 1
+    }
+
+    fun laneI5Gray(){
+        laneI5 = 1
+    }
+
+    fun laneN1Gray(){
+        laneN1 = 1
+    }
+
+    fun laneN2Gray(){
+        laneN2 = 1
+    }
+
+    fun laneN3Gray(){
+        laneN3 = 1
+    }
+
+    fun laneN4Gray(){
+        laneN4 = 1
+    }
+
+    fun laneN5Gray(){
+        laneN5 = 1
+    }
+
+    fun laneG1Gray(){
+        laneG1 = 1
+    }
+
+    fun laneG2Gray(){
+        laneG2 = 1
+    }
+
+    fun laneG3Gray(){
+        laneG3 = 1
+    }
+
+    fun laneG4Gray(){
+        laneG4 = 1
+    }
+
+    fun laneG5Gray(){
+        laneG5 = 1
+    }
+
+    fun laneO1Gray(){
+        laneO1 = 1
+    }
+
+    fun laneO2Gray(){
+        laneO2 = 1
+    }
+
+    fun laneO3Gray(){
+        laneO3 = 1
+    }
+
+    fun laneO4Gray(){
+        laneO4 = 1
+    }
+
+    fun laneO5Gray(){
+        laneO5 = 1
+    }
+
+    //Set to White Background
+    fun laneB1White(){
+        laneB1 = 0
+    }
+
+    fun laneB2White(){
+        laneB2 = 0
+    }
+
+    fun laneB3White(){
+        laneB3 = 0
+    }
+
+    fun laneB4White(){
+        laneB4 = 0
+    }
+
+    fun laneB5White(){
+        laneB5 = 0
+    }
+
+    fun laneI1White(){
+        laneI1 = 0
+    }
+
+    fun laneI2White(){
+        laneI2 = 0
+    }
+
+    fun laneI3White(){
+        laneI3 = 0
+    }
+
+    fun laneI4White(){
+        laneI4 = 0
+    }
+
+    fun laneI5White(){
+        laneI5 = 0
+    }
+
+    fun laneN1White(){
+        laneN1 = 0
+    }
+
+    fun laneN2White(){
+        laneN2 = 0
+    }
+
+    fun laneN3White(){
+        laneN3 = 0
+    }
+
+    fun laneN4White(){
+        laneN4 = 0
+    }
+
+    fun laneN5White(){
+        laneN5 = 0
+    }
+
+    fun laneG1White(){
+        laneG1 = 0
+    }
+
+    fun laneG2White(){
+        laneG2 = 0
+    }
+
+    fun laneG3White(){
+        laneG3 = 0
+    }
+
+    fun laneG4White(){
+        laneG4 = 0
+    }
+
+    fun laneG5White(){
+        laneG5 = 0
+    }
+
+    fun laneO1White(){
+        laneO1 = 0
+    }
+
+    fun laneO2White(){
+        laneO2 = 0
+    }
+
+    fun laneO3White(){
+        laneO3 = 0
+    }
+
+    fun laneO4White(){
+        laneO4 = 0
+    }
+
+    fun laneO5White(){
+        laneO5 = 0
+    }
+    
+    
+
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
